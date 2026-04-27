df4e9daca4e0628b575a868f20497b6cc00f69a2
Elias Cheng <elias.cheng@yahoo.de>
2026-04-27 08:44:05 +0800

    update: 實作控制層
     - 透過建構子注入 Service
     - 呼叫 Service 並根據結果回傳相對應的 `IActionResult`

--- RemittanceTest/Controllers/RemittanceController.cs
+++ RemittanceTest/Controllers/RemittanceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RemittanceTest.Models;
+using RemittanceTest.Services;
 
 namespace RemittanceTest.Controllers
 {
@@ -8,6 +9,12 @@ namespace RemittanceTest.Controllers
     public class RemittanceController : ControllerBase
     {
         // TODO: 1. 請透過建構子注入 (Constructor Injection) 引入 IRemittanceService
+        private readonly IRemittanceService _remittanceService;
+
+        public RemittanceController(IRemittanceService remittanceService)
+        {
+            _remittanceService = remittanceService;
+        }
 
         [HttpPost("{id}/cancel")]
         public IActionResult Cancel(int id)
@@ -15,7 +22,18 @@ namespace RemittanceTest.Controllers
             // TODO: 2. 呼叫 Service 執行取消邏輯
             // TODO: 3. 根據 Service 回傳的結果，回傳相對應的 HTTP 狀態碼 (Ok / BadRequest / NotFound)
 
-            return Ok();
+            var result = _remittanceService.CancelRemittance(id);
+
+            if (!result.IsSuccess)
+            {
+                if (result.Message.Contains("找不到"))
+                {
+                    return NotFound(new { message = result.Message });
+                }
+                return BadRequest(new { message = result.Message });
+            }
+
+            return Ok(new { message = result.Message });
         }
     }
 }
